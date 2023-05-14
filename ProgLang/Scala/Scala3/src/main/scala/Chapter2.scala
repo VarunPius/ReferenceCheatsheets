298bcc35493c3d76fd02616f3611822ab5f2a0f7
Varun Pius Rodrigues <varunpiusrodrix@gmail.com>
2023-05-14 13:23:54 -0700

    Adding file for DS

--- ProgLang/Scala/Scala3/src/main/scala/Chapter2.scala
+++ ProgLang/Scala/Scala3/src/main/scala/Chapter2.scala
@@ -1,3 +1,35 @@
+/***************************************************************************************************
+Chapter 2: Data and control Structures
+
+This chapter includes:
+  - Data structures
+  - Control structures
+***************************************************************************************************/
+
+
 object Chapter2 {
+  @main def dataStructures() =
+    // Val is used to initialize constant value
+    val num1 = 12
+    println("Value Num1: " + num1)
+
+    // We can use static type declaration too
+    val num2: Int = 23
+    println("Value Num2: " + num2)
+
+    //But remember if we use val, we have to initialize value; we can't leave it empty
+    // as val num2: Int; it expects a value
+
+    // To declare a variable you may use var:
+    var num3 = 12
+    println("Variable Num3 pre: " + num3)
+    num3 = 23
+    println("Variable Num3 post: " + num3)
+
+    //This results in an error: num3 = "test"
+    // Because datatype can't be changed; only value can be
+
+
+
 
 }
