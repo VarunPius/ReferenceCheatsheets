40d1125f54e85ee57721a66322bd1a394ae6fe38
Varun Pius Rodrigues <varunpiusrodrix@gmail.com>
2023-06-14 08:59:14 -0700

    Scala3 moving control structures

--- ProgLang/Scala/Scala3/src/main/scala/Chapter3.scala
+++ ProgLang/Scala/Scala3/src/main/scala/Chapter3.scala
@@ -9,7 +9,9 @@ This chapter includes:
 
 object Chapter3 {
   @main def mainChp3 =
-    println("Chapter 3: Methods")
+    println("Chapter 3: Methods & control structures")
+    controlStructures
+
     val sum1 = addNum(2,3)
     val sum2 = addNum2(4,5)
     println("Sum: " + sum1 + " | " + sum2)
@@ -20,6 +22,28 @@ object Chapter3 {
     val prod3 = prodNum2()
     println("Product: " + prod1 + " | " + prod2 + " | " + prod3)
 
+
+  def controlStructures =
+    println("--------------------------------------------------")
+    println("++++++++++ Control structures start here +++++++++")
+    println("--------------------------------------------------")
+
+    val x = 10
+    // If uses if-then or if-then-else format in scala
+    if x == 10 then println("if1: Equals")
+
+    if x == 10 then
+      println("If2: Equals again")
+      println("Value of x: " + x)
+
+    if x < 5 then
+      println("If3: Less than")
+    else if x > 20 then
+      println("If3: greater than")
+    else
+      println("If3: Equals")
+
+    // For Loops
 
 
   def addNum(a:Int, b:Int):Int=
