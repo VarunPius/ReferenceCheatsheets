c6f4c9ff91f0470aaba03f50599502724937f093
Varun Pius Rodrigues <varunpiusrodrix@gmail.com>
2023-10-21 12:49:46 -0700

    Go func 2

--- ProgLang/GoLang/chapter5.go
+++ ProgLang/GoLang/chapter5.go
@@ -18,10 +18,15 @@ func chapter5() {
 
     f1 := plus(2, 3)
     fmt.Println("Function: F1", f1)
+
+    f2 := plus2(3, 4, 5)
+    fmt.Println("Function: F2", f2)
 }
 
 func plus(a int, b int) int {
     return a + b
 }
 
-
+func plus2(a, b, c int) int {
+    return a + b + c
+}
