b02fe1f43948f79cd6f7cbe72921cada727e8661
Varun Pius Rodrigues <varunpiusrodrix@gmail.com>
2023-10-21 12:46:39 -0700

    go basic func

--- ProgLang/GoLang/chapter5.go
+++ ProgLang/GoLang/chapter5.go
@@ -4,3 +4,23 @@ Chapter 5:
     - Functions
 */
 
+package main
+
+import (
+    "fmt"
+)
+
+func chapter5() {
+    fmt.Println("------------------------------------------------")
+    fmt.Println("------------------ Chapter 4 -------------------")
+    fmt.Println("------------------------------------------------")
+    fmt.Println()
+
+    plus(2, 3)
+}
+
+func plus(a int, b int) int {
+    return a + b
+}
+
+
