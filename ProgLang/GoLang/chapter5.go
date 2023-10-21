62f2f27d89b05cf92d508ce1f9bfd73898715f7f
Varun Pius Rodrigues <varunpiusrodrix@gmail.com>
2023-10-21 12:48:16 -0700

    Go func update

--- ProgLang/GoLang/chapter5.go
+++ ProgLang/GoLang/chapter5.go
@@ -12,11 +12,12 @@ import (
 
 func chapter5() {
     fmt.Println("------------------------------------------------")
-    fmt.Println("------------------ Chapter 4 -------------------")
+    fmt.Println("------------------ Chapter 5 -------------------")
     fmt.Println("------------------------------------------------")
     fmt.Println()
 
-    plus(2, 3)
+    f1 := plus(2, 3)
+    fmt.Println("Function: F1", f1)
 }
 
 func plus(a int, b int) int {
