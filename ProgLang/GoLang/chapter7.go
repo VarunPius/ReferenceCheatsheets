74636294a57bea8afa5a9b6c7277d528764f064c
Varun Pius Rodrigues <varunpiusrodrix@gmail.com>
2023-12-08 11:31:39 -0800

    go string intro

---
+++ ProgLang/GoLang/chapter7.go
@@ -0,0 +1,23 @@
+/*
+Chapter 7:
+    includes:
+    - Strings
+    - Runes
+*/
+
+package main
+
+import (
+    "fmt"
+)
+
+func chapter7() {
+    fmt.Println("------------------------------------------------")
+    fmt.Println("------------------ Chapter 7 -------------------")
+    fmt.Println("------------------------------------------------")
+    fmt.Println()
+
+    
+    fmt.Println()
+
+}
