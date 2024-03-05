aba0f94133f0f89b6386e145e020e2f9b8101469
Varun Pius Rodrigues <varunpiusrodrix@gmail.com>
2024-03-04 19:28:32 -0800

    GoLang: rearranging files

--- ProgLang/GoLang/main.go
+++ ProgLang/GoLang/main.go
@@ -8,7 +8,9 @@ func main() {
     //chapter4()
     //chapter5()
     //chapter6()
-    chapter7()
+    //chapter7()
+    chapter08()
+
 }
 
 
@@ -32,4 +34,5 @@ TOC:
     - Chapter 5: Functions and closures
     - Chapter 6: Pointers
     - Chapter 7: Strings and Runes
+    - Chapter 8: Structs
 */
