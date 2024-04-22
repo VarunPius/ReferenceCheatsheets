9f7670c08b1e1ed2d745dc882cf7ea712b017055
Varun Pius Rodrigues <varunpiusrodrix@gmail.com>
2024-04-21 23:37:59 -0700

    GoLang error handling p1

--- ProgLang/GoLang/chapter09.go
+++ ProgLang/GoLang/chapter09.go
@@ -9,6 +9,7 @@ package main
 
 import (
     "fmt"
+    "errors"
     //"time"
 )
 
@@ -18,10 +19,24 @@ func chapter09() {
     fmt.Println("------------------------------------------------")
     fmt.Println()
 
-    //errorsExplanation()
+    v1, e1:= errorsExplanation1(12)
+    v2, e2:= errorsExplanation1(15)
+    fmt.Println("Errors1: Val: ", v1, v2)  //Errors1: Val:  17 -1
+    fmt.Println("Errors1: Err: ", e1, e2)  //Errors1: Err:  <nil> Can't work with 15
+
     fmt.Println()
     
     fmt.Println()
 
 }
 
+func errorsExplanation1(arg int) (int, error){
+    // By convention, Errors are the last return values with return type error
+
+    if arg == 15 {
+        // errors.New constructs a basic error value with the given error message.
+        return -1, errors.New("Can't work with 15")
+    }
+
+    return arg + 5, nil     // A nil value in the error position indicates that there was no error.
+}
