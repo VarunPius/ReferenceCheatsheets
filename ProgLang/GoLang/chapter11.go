5de247df65a8b868c82d5f6e87f60c9e5f3c9af6
Varun Pius Rodrigues <varunpiusrodrix@gmail.com>
2024-04-24 23:25:01 -0700

    GoLang buffered channel

--- ProgLang/GoLang/chapter11.go
+++ ProgLang/GoLang/chapter11.go
@@ -21,41 +21,81 @@ func chapter11() {
     channelsExplanation()
     fmt.Println()
 
-	channelBufferedExplanation()
-	fmt.Println()
+    channelsExplanation2()
+    fmt.Println()
+
+    channelBufferedExplanation()
+    fmt.Println()
 
 }
 
 func channelsExplanation() {
     // Channels are the pipes that connect concurrent goroutines.
-	// You can send values into channels from one goroutine and receive those values into another goroutine.
+    // You can send values into channels from one goroutine and receive those values into another goroutine.
 
-	messages := make(chan string)	// Create a new channel with make(chan val-type). Channels are typed by the values they convey.
-	go func() { messages <- "Ping"}()	
-	go func() { messages <- "Ping2"}()	
-	go func() { messages <- "Ping3"}()	
+    fmt.Println("Channel Explanation 1")
+    messages := make(chan string)    // Create a new channel with make(chan val-type). Channels are typed by the values they convey.
+    go func() { messages <- "Ping"}()    
+    go func() { messages <- "Ping2"}()    
+    go func() { messages <- "Ping3"}()    
 
-	msg := <- messages
-	msg2 := <- messages
-	msg3 := <- messages
-	fmt.Println("Message from channel: ", msg, msg2, msg3)	//Message from channel:  Ping3 Ping Ping2
-	// By default sends and receives block until both the sender and receiver are ready.
-	// This property allowed us to wait at the end of our program for the "ping" message without having to use any other synchronization.
-	// In the above example, every ping we sent we were able to send only because correesponding receive was available
-	// ? Though i was able to send even if corresponding receive was not available. Maybe they changed in newer version
+    msg := <- messages
+    msg2 := <- messages
+    msg3 := <- messages
+    fmt.Println("Message from channel: ", msg, msg2, msg3)    //Message from channel:  Ping3 Ping Ping2
+    // By default sends and receives block until both the sender and receiver are ready.
+    // This property allowed us to wait at the end of our program for the "ping" message without having to use any other synchronization.
+    // In the above example, every ping we sent we were able to send only because correesponding receive was available
+    // ? Though i was able to send even if corresponding receive was not available. Maybe they changed in newer version
 }
 
-func channelBufferedExplanation(){
-	// By default channels are unbuffered,
-	// meaning that they will only accept sends (chan <-)
-	// if there is a corresponding receive (<- chan) ready to receive the sent value.
-	// Buffered channels accept a limited number of values without a corresponding receiver for those values.
-	messages := make(chan string, 3)
-	messages <- "Buffered"
-	messages <- "Channel"
-	messages <- "Example"
+func channelsExplanation2() {
+    fmt.Println("Channel Explanation 2")
 
-	fmt.Println(<- messages)
-	fmt.Println(<- messages)
-	fmt.Println(<- messages)
+    messages := make(chan string)
+    go func() { messages <- "Ping"}()    
+
+    msg := <- messages
+    //msg2 := <- messages       // We can't do this if only one message is sent in channel
+
+    fmt.Println("Message from channel: ", msg)
+    //fmt.Println("Message from channel: ", msg2)
+    /*
+    Message from channel:  Ping
+    */
+
+    /*
+    Also, sending messages are done within goroutine
+    If you don't send withing goroutine, you will get the following error: fatal error: all goroutines are asleep - deadlock!
+    Only way to not do within goroutine is via buffered channel.
+    If the sending code is in the main function, and there's no other goroutine to receive the message, the program deadlocks.
+    Goroutines provide a separate execution context, preventing the main program from being blocked while waiting for a channel operation.
+
+    Here's a sample program that FAILS:
+    messages := make(chan string)
+    messages <- "Ping"
+    msg := <- messages
+    fmt.Println("Message from channel: ", msg)
+
+    Even if the receiver is in the very next line, it fails
+    */
+}
+
+
+func channelBufferedExplanation(){
+    fmt.Println("Bufffered Explanation")
+    // By default channels are unbuffered,
+    // meaning that they will only accept sends (chan <-)
+    // if there is a corresponding receive (<- chan) ready to receive the sent value.
+    // Buffered channels accept a limited number of values without a corresponding receiver for those values.
+    messages := make(chan string, 3)
+    messages <- "Buffered"
+    messages <- "Channel"
+    messages <- "Example"
+    fmt.Println(<- messages)
+    messages <- "Example2"
+
+    fmt.Println(<- messages)
+    fmt.Println(<- messages)
+    fmt.Println(<- messages)
 }
